ece46a4b85d71c2df3406b0348b79a52e3ee5ff4
vishal5394 <vishaljadyal@gmail.com>
2022-08-03 12:47:17 +0530

    Added: Validate The Email

--- Regex.java
+++ Regex.java
@@ -12,18 +12,31 @@ public class Regex {
         }else
             System.out.println(name+" is not valid");
 	  }
+	  
+	  public void validateEmail(String email){
+		          Pattern pattern=Pattern.compile("^[a-zA-Z0-9_'*+=~^.-]+@[a-zA-Z0-9.-]+$");
+		          boolean matches = pattern.matcher(email).matches();
+		          if (matches){
+		              System.out.println("email is valid");
+		          }else
+		              System.out.println("email is not valid");
+		      }
 
 public static void main(String[] args) {
 	    Regex regex=new Regex();
 	    Scanner scanner=new Scanner(System.in);
-	    System.out.println("Enter first name :");
+	    /*System.out.println("Enter first name :");
 	    String firstName=scanner.next();
 	    regex.validateName(firstName);
 	    
 
         System.out.println("\n Enter last name: ");
         String lastName=scanner.next();
-        regex.validateName(lastName);
+        regex.validateName(lastName);*/
+        
+        System.out.println("Enter email: ");
+        String email=scanner.next();
+        regex.validateEmail(email);
     
 	}
 	
