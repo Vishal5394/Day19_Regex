59b723c3b9da702c43c4f8dee7d28a2dfebce25f
vishal5394 <vishaljadyal@gmail.com>
2022-08-03 12:20:04 +0530

    Added: Validate The First Name

--- Regex.java
+++ Regex.java
@@ -1,8 +1,28 @@
 package com.BridgeLabz.Regex;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Regex {
-	public static void main(String[] args) {
-		System.out.println("Welome to Regex ");
-	}
+	public void validateFirstName(String firstName){
+			Pattern pattern=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+		    boolean matches = pattern.matcher(firstName).matches();
+		    if (matches){
+		        System.out.println("FirstName Is Valid");
+		    }
+		    else
+		        System.out.println("Please Enter valid FirstName ");
 }
+
+public static void main(String[] args) {
+	    Regex regex=new Regex();
+	    Scanner scanner=new Scanner(System.in);
+	    System.out.println("Enter first name :");
+	    String firstName=scanner.next();
+	    regex.validateFirstName(firstName);
+    
+	}
+	
+}
+
 
