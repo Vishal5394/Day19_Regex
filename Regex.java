a1511ed190d7343d22c70c2eb2f6ded7eb869b98
vishal5394 <vishaljadyal@gmail.com>
2022-08-03 12:26:21 +0530

    Added: Validate The First Name & Last Name

--- Regex.java
+++ Regex.java
@@ -4,22 +4,26 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Regex {
-	public void validateFirstName(String firstName){
-			Pattern pattern=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
-		    boolean matches = pattern.matcher(firstName).matches();
-		    if (matches){
-		        System.out.println("FirstName Is Valid");
-		    }
-		    else
-		        System.out.println("Please Enter valid FirstName ");
-}
+	  public void validateName(String name){
+        Pattern pattern=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+        boolean matches = pattern.matcher(name).matches();
+        if (matches){
+            System.out.println(name+" is valid");
+        }else
+            System.out.println(name+" is not valid");
+	  }
 
 public static void main(String[] args) {
 	    Regex regex=new Regex();
 	    Scanner scanner=new Scanner(System.in);
 	    System.out.println("Enter first name :");
 	    String firstName=scanner.next();
-	    regex.validateFirstName(firstName);
+	    regex.validateName(firstName);
+	    
+
+        System.out.println("\n Enter last name: ");
+        String lastName=scanner.next();
+        regex.validateName(lastName);
     
 	}
 	
